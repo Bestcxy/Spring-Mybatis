83aa4d17db1b7e390287653be0a9e2c296b6f2b8
wujie <snailknight@yeah.net>
2017-11-11 12:21:00 +0800

    增加 事物配置文件 applicationContxt-trans.xml

--- springmybatis/src/test/java/com/bestcxx/stu/springmybatis/service/TestTableOneServiceTest.java
+++ springmybatis/src/test/java/com/bestcxx/stu/springmybatis/service/TestTableOneServiceTest.java
@@ -1,11 +1,14 @@
 package com.bestcxx.stu.springmybatis.service;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.bestcxx.stu.springmybatis.model.TestTableOne;
@@ -15,7 +18,7 @@ import com.bestcxx.stu.springmybatis.model.TestTableOne;
 @ContextConfiguration(locations={"classpath:applicationContext.xml"})  
 //@TransactionConfiguration(transactionManager = "defaultTransactionManager",defaultRollback=false)//事务管理    
 @Rollback(true)
-public class TestTableOneServiceTest {
+public class TestTableOneServiceTest extends AbstractTransactionalJUnit4SpringContextTests{
 	@Autowired
 	private TestTableOneService testTableOneService;
 	
@@ -24,5 +27,20 @@ public class TestTableOneServiceTest {
 		Integer id=1;
 		TestTableOne t=testTableOneService.selectByPrimaryKey(id);
 		System.out.println("搜索结果："+t.getComment());
+	}
+	
+	@Test
+	public void testInsert(){
+		Integer id=1;
+		TestTableOne record=new TestTableOne();
+		record.setId(34);
+		record.setComment("测试");
+		try {
+			testTableOneService.insert(record);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
